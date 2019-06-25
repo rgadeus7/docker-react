f21ce5064d274d20f963a162d3c7924747e3a87c
Rakesh Reddy Gade <rakeshreddy.gade@cvshealth.com>
2019-06-25 19:14:10 -0400

    pushing new feature changes

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> - I am changed
+          Edit <code>src/App.js</code> - I am changed by new feature branch for testing
         </p>
         <a
           className="App-link"
